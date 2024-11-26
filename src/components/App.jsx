7bf39b3f26eef074458be1b4155f4debe2727f5d
panawita <162113789+panawita@users.noreply.github.com>
2024-11-26 01:15:09 +0100

    Added local storage functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,45 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ContactForm from './ContactForm';
 import { nanoid } from 'nanoid';
 import SearchBox from './SearchBox';
 import ContactList from './ContactList';
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    return savedContacts
+      ? JSON.parse(savedContacts)
+      : [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ];
+  });
 
   const [filter, setFilter] = useState('');
+
+  // Or:
+  // const [contacts, setContacts] = useState([]);
+  // const [filter, setFilter] = useState('');
+
+  // useEffect(() => {
+  //   const savedContacts = localStorage.getItem('contacts');
+  //   if (savedContacts) {
+  //     setContacts(JSON.parse(savedContacts));
+  //   } else {
+  //     setContacts([
+  //       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //     ]);
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = (name, number) => {
     const newContact = {
